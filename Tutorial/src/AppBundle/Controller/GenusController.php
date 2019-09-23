7aa144e8f01fa71e411c7026d5a9dc84e5bf5fac
nxasana <ncukanaathandiwe@gmail.com>
2019-09-23 09:17:29 +0200

    All of these

--- Tutorial/src/AppBundle/Controller/GenusController.php
+++ Tutorial/src/AppBundle/Controller/GenusController.php
@@ -2,42 +2,90 @@
 
 namespace AppBundle\Controller;
 
-// use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\Genus;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class GenusController extends Controller
 {
     /**
-     * @Route("/genus/{genusName}")
+     * @Route("/genus/new")
      */
-    public function showAction($genusName)
+    public function newAction()
     {
-        // replace this example code with whatever you need
+        $genus = new Genus();
+        $genus->setName('Octopus'.rand(1, 100));
+        $genus->setSubFamily('Octopodinae');
+        $genus->setSpeciesCount(rand(100, 99999));
 
-            $funFact = 'Octopuses can change the color of their body in just *three-tenths* of a second!';
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($genus);
+        $em->flush();
 
-            $funFact = $this->get('markdown.parser')
-            ->transform($funFact);
-
-        return $this->render('genus/show.html.twig', [
-            'name'=> $genusName,
-            'funFact' => $funFact
-        ]);
-       
-           
+        return new Response('<html><body>Genus created!</body></html>');
     }
 
     /**
-     * @Route("/genus/{genusName}/notes", name ="genus_show_notes")
+     * @Route("/genus")
+     */
+    public function listAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $genuses = $em->getRepository('AppBundle:Genus')
+            ->findAllPublishedOrderedBySize();
+
+        return $this->render('genus/list.html.twig', [
+            'genuses' => $genuses
+        ]);
+    }
+
+    /**
+     * @Route("/genus/{genusName}", name="genus_show")
+     */
+    public function showAction($genusName)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $genus = $em->getRepository('AppBundle:Genus')
+            ->findOneBy(['name' => $genusName]);
+
+        if (!$genus) {
+            throw $this->createNotFoundException('genus not found');
+        }
+
+        // todo - add the caching back later
+        /*
+        $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
+        $key = md5($funFact);
+        if ($cache->contains($key)) {
+            $funFact = $cache->fetch($key);
+        } else {
+            sleep(1); // fake how slow this could be
+            $funFact = $this->get('markdown.parser')
+                ->transform($funFact);
+            $cache->save($key, $funFact);
+        }
+        */
+
+        $this->get('logger')
+            ->info('Showing genus: '.$genusName);
+
+        return $this->render('genus/show.html.twig', array(
+            'genus' => $genus
+        ));
+    }
+
+    /**
+     * @Route("/genus/{genusName}/notes", name="genus_show_notes")
      * @Method("GET")
      */
     public function getNotesAction($genusName)
     {
-       $notes = [
+        $notes = [
             ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
             ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
             ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
@@ -45,6 +93,7 @@ class GenusController extends Controller
         $data = [
             'notes' => $notes
         ];
+
         return new JsonResponse($data);
     }
 }
